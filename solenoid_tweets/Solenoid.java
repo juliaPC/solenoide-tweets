9b795a82ad73ff8e9cafd35ed9ab991fba158d48
Miguel Colom <miguel@mcolom.info>
2015-10-07 07:03:47 +0200

    [Solenoid.java] reorganized pin control

--- solenoid_tweets/Solenoid.java
+++ solenoid_tweets/Solenoid.java
@@ -13,37 +13,62 @@ import processing.serial.*; // serial communication library
 
 
 public class Solenoid {
-  private Serial arduinoPort = null; // serial port object
-  private boolean do_hit;
-  private RandomAccessFile pin_file;
+  private boolean do_hit = false;
+  private RandomAccessFile pin_file = null;
+  private byte[] bytes_pin = null;
 
   public Solenoid(PApplet p_applet, String pin, boolean do_hit) {
     this.do_hit = do_hit;
-    
-    // GPIO config
+
     if (do_hit) {
+      // GPIO config
+      unexport_pin();
+      export_pin();
+
+      // Open file to control solenoid device using the GPIO pin
       try {
-		byte[] bytes_str = pin.getBytes();
-
-        RandomAccessFile f = new RandomAccessFile("/sys/class/gpio/unexport", "rw");
-        f.writeByte(bytes_str[0]);
-        if (bytes_str.length > 1)
-          f.writeByte(bytes_str[1]);
-        f.close();        
-
-        f = new RandomAccessFile("/sys/class/gpio/export", "rw");
-		f.writeByte(bytes_str[0]);
-        if (bytes_str.length > 1)
-          f.writeByte(bytes_str[1]);
-        f.close();        
+        this.pin_file = new RandomAccessFile("/sys/class/gpio/gpio" + pin + "/value", "rw");
+        this.activate(false);
       }
       catch (FileNotFoundException e) {
         System.out.println("FileNotFoundException: " + e);
       }
       catch (IOException e) {
         System.out.println("IOException: " + e);
-      }              
+      }
     }
+  }
+
+  private void export_pin() {
+      try {
+        RandomAccessFile f = new RandomAccessFile("/sys/class/gpio/export", "rw");
+        f.writeByte(this.bytes_pin[0]);
+        if (this.bytes_pin.length > 1)
+          f.writeByte(this.bytes_pin[1]);
+        f.close();
+      }
+      catch (FileNotFoundException e) {
+        System.out.println("FileNotFoundException: " + e);
+      }
+      catch (IOException e) {
+        System.out.println("IOException: " + e);
+      }
+  }
+
+  private void unexport_pin() {
+      try {
+        RandomAccessFile f = new RandomAccessFile("/sys/class/gpio/unexport", "rw");
+        f.writeByte(this.bytes_pin[0]);
+        if (this.bytes_pin.length > 1)
+          f.writeByte(this.bytes_pin[1]);
+        f.close();
+      }
+      catch (FileNotFoundException e) {
+        System.out.println("FileNotFoundException: " + e);
+      }
+      catch (IOException e) {
+        System.out.println("IOException: " + e);
+      }
   }
 
   public void delay() {
@@ -55,30 +80,41 @@ public class Solenoid {
     }
   }
 
+  private void activate(boolean status) {
+    try {
+      if (status)
+        this.pin_file.writeByte((byte)'1');
+      else
+        this.pin_file.writeByte((byte)'0');
+    }
+    catch (FileNotFoundException e) {
+      System.out.println("FileNotFoundException: " + e);
+    }
+    catch (IOException e) {
+      System.out.println("IOException: " + e);
+    }
+  }
+
   public void hit() {
     if (this.do_hit) {
       System.out.println("Solenoid hit!");
 
-      try {
-        RandomAccessFile f = new RandomAccessFile("/sys/class/gpio/gpio18/value", "rw");
-        f.writeByte((byte)'1');
-        f.close();        
-
-        this.delay();
-
-        f = new RandomAccessFile("/sys/class/gpio/gpio18/value", "rw");
-        f.writeByte((byte)'0');
-        f.close();        
-      }
-      catch (FileNotFoundException e) {
-        System.out.println("FileNotFoundException: " + e);
-      }
-      catch (IOException e) {
-        System.out.println("IOException: " + e);
-      }
+      this.activate(true);
+      this.delay();
+      this.activate(false);
     } else {
       System.out.println("Solenoid simulated hit!");
     }
   }
-}
+  
+  // Free sources before finishing
+  public void finalize() throws IOException {
+	// Close pin file
+	if (this.pin_file != null)
+        this.pin_file.close();
 
+	// Unexport pin so it can be used by others
+    if (do_hit)
+	  this.unexport_pin();
+  }
+}
