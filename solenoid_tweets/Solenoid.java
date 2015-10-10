d2a46b50a2d92eff6dde361ed0846e9866a21cfd
Miguel Colom <miguel@mcolom.info>
2015-10-10 20:09:45 +0200

    [Solenoid.java] Implemented tweets counter queue

--- solenoid_tweets/Solenoid.java
+++ solenoid_tweets/Solenoid.java
@@ -13,16 +13,54 @@ import processing.core.*;
 import processing.serial.*; // serial communication library
 
 
-public class Solenoid {
+public class Solenoid implements Runnable {
   private String pin;
   private boolean do_hit = false;
-  private List bytes_pin;
+  private List bytes_pin = new ArrayList<Byte>();
+  private int pending = 0; // number of pending solenoid hits
+  private Thread th = null;
+
+  //public synchronized void add_hit() {
+  public synchronized void add_hit() {
+	  this.pending++;
+
+	  // Avoid too many enqueued tweets
+	  if (this.pending > 100)
+	    this.pending = 10;
+  }
+
+  public synchronized void done_hit() {
+	  this.pending--;
+  }
+
+  // Thread run
+  public void run() {
+
+    while (true) {
+	  // Process pending tweets
+	  if (this.pending > 0) {
+        // Solenoid cycle
+
+        int half_cycle_time_ms = 100;  // [ToDo]: read from config
+
+        this.activate(true);
+        this.delay(half_cycle_time_ms);
+        this.activate(false);
+        this.delay(half_cycle_time_ms);
+
+        this.done_hit();
+      }
+
+      // Prevent CPU at 100%
+	  this.delay(10);
+	}
+  }
 
   public Solenoid(PApplet p_applet, String pin, boolean do_hit) {
     this.do_hit = do_hit;
     this.pin = pin;
     
-    this.bytes_pin = new ArrayList<Byte>();
+    //this.bytes_pin = new ArrayList<Byte>();
     this.bytes_pin.add(new Byte((byte)(pin.charAt(0))));
     
     if (pin.length() > 1)
@@ -34,6 +72,9 @@ public class Solenoid {
       export_pin();
 
       this.activate(false);
+
+      this.th = new Thread(this, "solenoid_thread");
+      this.th.start();
     }
   }
 
@@ -80,9 +121,9 @@ public class Solenoid {
       }
   }
 
-  public void delay() {
+  public void delay(int ms) {
     try {
-      Thread.sleep(200); // ms
+      Thread.sleep(ms);
     }
     catch (InterruptedException e) {
       System.out.println("solenoid delay: " + e);
@@ -107,16 +148,14 @@ public class Solenoid {
     }
   }
 
+  // Hit notification
   public void hit() {
     if (this.do_hit) {
       System.out.println("Solenoid hit!");
-
-      this.activate(true);
-      this.delay();
-      this.activate(false);
-    } else {
-      System.out.println("Solenoid simulated hit!");
+      this.add_hit();
     }
+    else
+      System.out.println("Solenoid simulated hit!");
   }
   
   // Free sources before finishing
@@ -124,6 +163,10 @@ public class Solenoid {
     // Unexport pin so it can be used by others
     if (do_hit)
 	  this.unexport_pin();
+
+	// Stop thread
+	if (this.th != null)
+	  this.th.stop();
   }
 }
 
